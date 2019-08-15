51a56cf35a0784f0dcaeb8a77968ac6783714c89
Juba <rgjuba@gmail.com>
2019-08-15 14:52:42 -0300

    listas vetores e conjuntos

---
+++ src/padwan/core.clj
@@ -0,0 +1,57 @@
+(ns padwan.core)
+
+;funcao defn cria uma nova função, no caso a Foo
+(defn foo
+  "Este é o lado Clojure da força!."
+  [x]
+  (println x "Bem vindo, jovem Padawan!"))
+
+;Função zero? verifica se o numero é exatamente zero e mod traz o resto da divisão
+(defn divisivel-por?
+  [dividendo divisor]
+  (zero? (mod dividendo divisor))
+  )
+
+;Funcao and verifica se as duas condicoes sao verdadeiras
+(defn fizzbuzz
+  [num]
+  (cond (and (divisivel-por? num 3) (divisivel-por? num 5)) "fizzbuzz"
+        (divisivel-por? num 3) "fizz"
+        (divisivel-por? num 5) "buzz"
+        :else num)
+  )
+
+;def define uma constante
+;range é uma função que percorre o espaço informado entre dois numeros
+;criacao de uma lista
+(def um-ate-quinze (range 1 16))
+
+;list cria uma lista de elementos
+(def cantora-arretada (list "Renata Arruda" "Joao Pessoa" 23 "dezembro" 1967))
+
+;funcao nth retorna a posicao informada em uma lista
+(nth cantora-arretada 4)
+
+;retorna a primeira posicao
+(first cantora-arretada)
+
+;funcao vector cria um vetor
+(def cantor-arretado (vector "Chico cesar" "Catole da Rocha" 26 "janeiro" 1964))
+
+;funcao get retorna a posicao informada em um vetor
+(get cantor-arretado 0)
+
+;retorna a ultima posicao
+(last cantor-arretado)
+
+;funcao conj adiciona elementos em uma lista ou array
+(conj cantor-arretado "MPB")
+
+;funcao hash-set cria uma lista de valores unicos
+(def artistas #{"Renata Arruda" "Chico Cesar"})
+
+
+
+
+
+
